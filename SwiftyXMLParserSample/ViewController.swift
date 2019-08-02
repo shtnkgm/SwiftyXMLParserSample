8f3873024153f339f6f5877aa42e77750329c7eb
shtnkgm <shtnkgm@gmail.com>
2019-08-03 00:38:25 +0900

    Initial Commit

---
+++ SwiftyXMLParserSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyXMLParserSample
+//
+//  Created by Shota Nakagami on 2019/08/03.
+//  Copyright © 2019 Shota Nakagami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
